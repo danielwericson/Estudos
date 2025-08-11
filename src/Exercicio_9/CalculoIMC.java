ec5f59a176231d9f4187f90a66face47beee9e2e
Daniel Wericson <danielwericson@live.com>
2025-08-11 11:46:42 -0300

    Inclusão do exercicio 9

--- src/Exercicio_9/CalculoIMC.java
+++ src/Exercicio_9/CalculoIMC.java
@@ -1,7 +1,9 @@
+package Exercicio_9;
+
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class CalculoIMC {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
@@ -15,9 +17,9 @@ public class Main {
         double altura = sc.nextDouble();
         System.out.println();
         double imc = peso / Math.pow(altura, 2);
-        System.out.printf("Seu IMC é: %.2f%n",imc);
+        System.out.printf("Seu IMC é: %.2f%n", imc);
 
-        if (imc < 18){
+        if (imc < 18) {
             System.out.println("Abaixo do peso");
         } else if (imc < 24.9) {
             System.out.println("Peso ideal (parabéns!)");
@@ -25,7 +27,7 @@ public class Main {
             System.out.println("Levemente acima do peso");
         } else if (imc < 34.9) {
             System.out.println("Obesidade grau  I");
-        } else if (imc < 39.9){
+        } else if (imc < 39.9) {
             System.out.println("Obesidade grau II (severa)");
         } else {
             System.out.println("Obesidade grau III (morbida)");
