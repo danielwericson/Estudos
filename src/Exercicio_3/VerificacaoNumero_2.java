00d10bece02bdd16d0eae1958bb848f0815c8c0b
Daniel Wericson <danielwericson@live.com>
2025-08-11 13:14:01 -0300

    adicionando o exercicio_3

---
+++ src/Exercicio_3/VerificacaoNumero_2.java
@@ -0,0 +1,25 @@
+package Exercicio_3;
+
+import java.util.Scanner;
+
+public class VerificacaoNumero_2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        int A = sc.nextInt();
+        System.out.print("Digite outro numero: ");
+        int B = sc.nextInt();
+
+        if (A == B){
+            int C = A + B;
+            System.out.println("Resultado: " + C);
+        } else {
+            int C = A * B;
+            System.out.println("Resultado: " + C);
+        }
+    }
+}
+
