fcb2ea3addbc99ef6ec8a2f9ab075903a905e71b
Daniel Wericson <danielwericson@live.com>
2025-08-11 13:10:45 -0300

    adicionando o exercicio_2

---
+++ src/Exercicio_2/PositivoNegativo.java
@@ -0,0 +1,26 @@
+package Exercicio_2;
+
+import java.util.Scanner
+
+public class PositivoNegativo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        int numero = sc.nextInt();
+
+
+        if (numero % 2 == 0) {
+            System.out.println("O numero é par");
+        } else {
+            System.out.println("O numero é impar");
+        }
+
+        if (numero % 2 > 0) {
+            System.out.println("O numero é positivo");
+        } else {
+            System.out.println("O numero é negativo");
+        }
+    }
+}
