8481248b56c6e1bfd13eaf251fdcb6a41592419e
Daniel Wericson <danielwericson@live.com>
2025-08-19 11:21:19 -0300

    Adicionando o exercicio_19

---
+++ src/Exercicio_19/Tabuada.java
@@ -0,0 +1,62 @@
+package Exercicio_19;
+
+
+
+public class Tabuada {
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        for (int i = 0; i <= n; i++){
+            System.out.println(1 + "x" + (i) + " = " + (i));
+        }
+        System.out.println();
+
+        for (int i = 0; i <= n; i++){
+            System.out.println(2 + "x" + (i) + " = " + (i * 2));
+        }
+        System.out.println();
+
+        for (int i = 0; i <= n; i++){
+            System.out.println(3 + "x" + (i) + " = " + (i * 3));
+        }
+        System.out.println();
+
+        for (int i = 0; i <= n; i++){
+            System.out.println(4 + "x" + (i) + " = " + (i * 4));
+        }
+        System.out.println();
+
+        for (int i = 0; i <= n; i++){
+            System.out.println(5 + "x" + (i) + " = " + (i * 5));
+        }
+        System.out.println();
+
+        for (int i = 0; i <= n; i++){
+            System.out.println(6 + "x" + (i) + " = " + (i * 6));
+        }
+        System.out.println();
+
+        for (int i = 0; i <= n; i++){
+            System.out.println(7 + "x" + (i) + " = " + (i * 7));
+        }
+        System.out.println();
+
+        for (int i = 0; i <= n; i++){
+            System.out.println(8 + "x" + (i) + " = " + (i * 8));
+        }
+        System.out.println();
+
+        for (int i = 0; i <= n; i++){
+            System.out.println(9 + "x" + (i) + " = " + (i * 9));
+        }
+        System.out.println();
+
+        for (int i = 0; i <= n; i++){
+            System.out.println(10 + "x" + (i) + " = " + (i * 10));
+        }
+        System.out.println();
+
+
+    }
+}
